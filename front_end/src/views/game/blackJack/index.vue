c5dccb0b3c97b4f7fbc8b48c9c80df40284f195f
lee <Lee0609x@163.com>
2023-12-06 17:55:03 +0800

    前端加入pixi

--- front_end/src/views/game/blackJack/index.vue
+++ front_end/src/views/game/blackJack/index.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
     <el-button type="primary" @click="onlinePlay">匹配对手</el-button>
+    <canvas id="pixi"></canvas>
   </div>
 </template>
 <script>
+//import * as PIXI from 'pixi.js'
 export default {
+  name: "ConnectionsLayer",
+  mounted() {
+//    this.drawPixi();
+  },
   data() {
     return {
-      gameClient: null
+      gameClient: null,
     }
   },
   methods: {
@@ -27,7 +33,28 @@ export default {
       } else {
         console.log('游戏通讯已经开启，无法创建新的通讯');
       }
-    }
+    },
+    // drawPixi() {
+    //   var canvas = document.getElementById('pixi')
+
+    //   const app = new PIXI.Application({
+    //     width: window.innerWidth,
+    //     height: window.innerHeight,
+    //     antialias: true,
+    //     transparent: true,
+    //     view: canvas,
+    //   })
+
+    //   let graphics = new PIXI.Graphics() 
+    //   graphics.lineStyle(8, 0x000000)
+
+    //   //start
+    //   graphics.moveTo(300, 250)
+    //   //end
+    //   graphics.lineTo(500, 250)
+
+    //   app.stage.addChild(graphics)
+    // },
   }
 }
 </script>
